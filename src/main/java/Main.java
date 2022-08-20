9fe3a3f63563a8ce1de5647dac3a10820528dfdf
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-20 16:24:46 +0300

    Решения и рефакторинг кода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         boolean isFormatted = false;
@@ -5,5 +9,44 @@ public class Main {
         // исправили теперь build succcessful
         if (isFormatted) {
         }
+        String test = "(])";
+        String test2 = "()[]{}";
+        String test3 = "(]";
+
+
+        System.out.println(isValid(test));
+        System.out.println(isValid(test2));
+        System.out.println(isValid(test3));
+    }
+
+    public static boolean isValid(String s) {
+
+
+        Set<Character> leftSymbol = Set.of('(', '[', '{');
+        Set<Character> rightSymbol = Set.of(')', ']', '}');
+
+        if (s.length() < 1 || rightSymbol.contains(s.charAt(0))) {
+
+            return false;
+
+        }
+
+        Deque<Character> leftStack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (leftSymbol.contains(c)) {
+                leftStack.addFirst(c);
+            } else if (c == ')' && !leftStack.isEmpty() && leftStack.peek() == '(') {
+                leftStack.poll();
+            } else if (c == ']' && !leftStack.isEmpty() && leftStack.peek() == '[') {
+                leftStack.poll();
+            } else if (c == '}' && !leftStack.isEmpty() && leftStack.peek() == '{') {
+                leftStack.poll();
+            } else {
+                return false;
+            }
+        }
+        return leftStack.size() == 0;
     }
 }
