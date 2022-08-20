46ee8f4bb1646e259a4c3a1ad8e4f2e176aa9584
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-20 14:21:24 +0300

    Подготовка переменных

--- Program.java
+++ Program.java
@@ -1,15 +1,29 @@
+import java.util.Deque;
+import java.util.Set;
+
 public class Program {
     public static void main(String[] args) {
         String test = "()";
         String test2 = "()[]{}";
         String test3 = "(]";
 
+
         System.out.println(isValid(test));
         System.out.println(isValid(test2));
         System.out.println(isValid(test3));
     }
+
     public boolean isValid(String s) {
 
+        Set<String> LSymbol = Set.of("(", "[", "{");
+        Set<String> RSymbol = Set.of(")", "]", "}");
+
+        Deque<Character> LStack;
+        Deque<Character> RStack;
+
+        for (char c : s.toCharArray()) {
+
+        }
         return false;
     }
 }
